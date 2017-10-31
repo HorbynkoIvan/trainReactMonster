30293099349ad8c20ae4f3aa50b41be1c9af75f9
Oleg <Pasechnik.o.n@gmail.com>
2017-10-31 21:02:27 +0200

    work with form

--- src/Registration/RegistrationForm.js
+++ src/Registration/RegistrationForm.js
@@ -6,8 +6,20 @@ class RegistrationForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email:''
-        }
+            email: ''
+        },
+            this.handleEmailChange = this.handleEmailChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("form is submited. Email value is: " + this.state.email)
+    };
+
+    handleEmailChange(event) {
+        console.log('e-mail was changed', event.target.value);
+        this.setState({email: event.target.value})
     }
 
     render() {
@@ -18,7 +30,9 @@ class RegistrationForm extends Component {
                     <input type="text"
                            placeholder="E-mail"
                            value={this.state.email}
+                           onChange={this.handleEmailChange}
                     />
+                    <button>Submit</button>
                 </form>
             </div>
         );
