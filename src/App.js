0098abc5506b5f74808b9fbbfb4bee9b9cfde707
Oleg <Pasechnik.o.n@gmail.com>
2017-10-31 21:40:13 +0200

    work with refs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Dropdown from './Dropdown/Dropdown'
 import Header from "./Header/Header";
 import RegistrationForm from "./Registration/RegistrationForm";
+import './style/app.css';
 
 const menu = [
     {
@@ -17,13 +18,23 @@ const menu = [
 ];
 
 class App extends Component {
+    submit() {
+        console.log('submit', this.testInput.value)
+    }
+
     render() {
         return (
-            <div>
+            <div className='container'>
                 <Header title="lesson about props" items={menu}/>
                 <Dropdown/>
                 <hr/>
                 <RegistrationForm/>
+                <hr/>
+                <div>
+                    <h5>work with attribute refs</h5>
+                    <input type="text" placeholder='test' ref={(input) => this.testInput = input}/>
+                    <button onClick={this.submit.bind(this)}>Submit</button>
+                </div>
             </div>
         );
     }
