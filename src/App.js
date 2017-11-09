1d53e63c191fb485142493b4a2e9961deb84092b
Ivan <horbynko.i@gmail.com>
2017-11-09 22:51:33 +0200

    performed find track and filter track

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.addTrack = this.addTrack.bind(this);
+        this.findTrack = this.findTrack.bind(this);
     }
 
     submit() {
@@ -32,11 +33,16 @@ class App extends Component {
         console.log(`trackInput: ${this.trackInput.value}`);
 
         this.props.onAddTrack(this.trackInput.value);
-        this.trackInput.value='';
+        this.trackInput.value = '';
+    }
+
+    findTrack() {
+        console.log(`findTrack: ${this.searchInput.value}`);
+        this.props.onFindTrack(this.searchInput.value)
     }
 
     render() {
-        console.log(`testState ${this.props.testState}`);
+        console.log(`tracks ${this.props.tracks}`);
         return (
             <div className='container'>
                 <Header title="lesson about props" items={menu}/>
@@ -49,12 +55,20 @@ class App extends Component {
                     <input type="text" placeholder='test' ref={(input) => this.testInput = input}/>
                     <button onClick={this.submit.bind(this)}>Submit</button>
                 </div>
-                <h5>redux connect</h5>
-                <input type="text" ref={(input) => this.trackInput = input}/>
-                <button onClick={this.addTrack}>Add track</button>
+                <h3>Redax</h3>
+                <div>
+                    <h4>Add track</h4>
+                    <input type="text" ref={(input) => this.trackInput = input}/>
+                    <button onClick={this.addTrack}>Add track</button>
+                </div>
+                <div>
+                    <h4>Find track</h4>
+                    <input type="text" ref={(input) => this.searchInput = input}/>
+                    <button onClick={this.findTrack}>Find track</button>
+                </div>
                 <ul>
-                    {this.props.testState.map((track, index) =>
-                        <li key={index}>{track}</li>
+                    {this.props.tracks.map((track, index) =>
+                        <li key={index}>{track.name}</li>
                     )}
                 </ul>
             </div>
@@ -64,11 +78,18 @@ class App extends Component {
 
 export default connect(
     state => ({
-        testState: state
+        tracks: state.tracks.filter(track => track.name.includes(state.filterTracks))
     }),
     dispatch => ({
-        onAddTrack: (trackName) => {
-            dispatch({type: 'ADD_TRACK', payload: trackName})
+        onAddTrack: (name) => {
+            const payload = {
+                id: Date.now().toString(),
+                name
+            };
+            dispatch({type: 'ADD_TRACK', payload})
+        },
+        onFindTrack: (name) => {
+            dispatch({type: 'FIND_TRACK', payload: name})
         }
     })
 )(App);
